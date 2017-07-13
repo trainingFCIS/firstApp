a2cd1222c73f49abe7d258c5251f2c25b37da6c2
mohammed mahmoud <mohammed mahmoud>
2017-07-13 21:36:00 +0200

    initial commit

---
+++ app/src/main/java/com/mahmoud/mohammed/firstapp/Main3Activity.java
@@ -0,0 +1,18 @@
+package com.mahmoud.mohammed.firstapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by siko on 7/13/2017.
+ */
+
+public class Main3Activity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
